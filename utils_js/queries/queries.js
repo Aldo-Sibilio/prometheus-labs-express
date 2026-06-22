8c5142a746f09d936987dcf4a83ec606a7bf4084
“Nilde <nilde.benedetti@gmail.com>
2026-06-22 11:36:30 +0200

    aggiunte queries reali per products (index, show)

--- utils_js/queries/queries.js
+++ utils_js/queries/queries.js
@@ -4,11 +4,14 @@ usiamo la sintassi */
 /*======== PRODUCTS ========*/
 
 const querySelectAllProducts = `
-inserisci qui la query
-`;
+select p.id, p.name, p.slug, p.short_description as shortDescription, p.marketing_description as mktgDescription, p.price_full as price, p.ingredients, p.created_at as createdAt, p.updated_at as updatedAt, p.image_main_url as imgMain, p.image_lifestyle as imgLifestyle, p.image_ksp as imgKsp
+from products p`
+;
 
 const querySelectProductBySlug = `
-inserisci qui la query
+select p.id, p.name, p.slug, p.short_description as shortDescription, p.marketing_description as mktgDescription, p.price_full as price, p.ingredients, p.created_at as createdAt, p.updated_at as updatedAt, p.image_main_url as imgMain, p.image_lifestyle as imgLifestyle, p.image_ksp as imgKsp
+from products p
+where p.slug = '?';
 `;
 
 const queries = {
