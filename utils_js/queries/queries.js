0fef035cc0e035dd0dad4d855c65cb3e9f6f2c93
“Nilde <nilde.benedetti@gmail.com>
2026-06-22 11:51:22 +0200

    spostate e valorizzate queries per rotte index e show di categories controller

--- utils_js/queries/queries.js
+++ utils_js/queries/queries.js
@@ -6,17 +6,39 @@ usiamo la sintassi */
 const querySelectAllProducts = `
 select p.id, p.name, p.slug, p.short_description as shortDescription, p.marketing_description as mktgDescription, p.price_full as price, p.ingredients, p.created_at as createdAt, p.updated_at as updatedAt, p.image_main_url as imgMain, p.image_lifestyle as imgLifestyle, p.image_ksp as imgKsp
 from products p`
-;
+    ;
 
 const querySelectProductBySlug = `
 select p.id, p.name, p.slug, p.short_description as shortDescription, p.marketing_description as mktgDescription, p.price_full as price, p.ingredients, p.created_at as createdAt, p.updated_at as updatedAt, p.image_main_url as imgMain, p.image_lifestyle as imgLifestyle, p.image_ksp as imgKsp
 from products p
-where p.slug = '?';
+where p.slug = ?;
 `;
+
+/*======== CATEGORIES ========*/
+
+const querySelectAllCategories = `
+ select c.id, c.name, c.slug
+from categories c;
+`;
+
+
+const querySelectCategoriesBySlug = `
+select c.id, c.name, c.slug
+from categories c
+where slug = ? `;
+
+
+/*======== POWERS ========*/
+
+
+
+
 
 const queries = {
     querySelectAllProducts,
-    querySelectProductBySlug
+    querySelectProductBySlug,
+    querySelectAllCategories,
+    querySelectCategoriesBySlug
 };
 
 export default queries;
